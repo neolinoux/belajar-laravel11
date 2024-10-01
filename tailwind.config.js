6d67a24da562f9dccd50cb780bae3731cefbf9ed
Rahmad Setiya Budi <222011623@stis.ac.id>
2024-10-01 21:58:58 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      extend: {
+        fontFamily: {
+          sans: ["Inter-Var", ...defaultTheme.fontFamily.sans],
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
