06778bac033bcbce7ca554f205d966bed7191b82
Rahmad Setiya Budi <222011623@stis.ac.id>
2024-10-02 09:13:38 +0800

    part 5 wpu

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['header' => 'Home Page']);
 });
 
 
 Route::get('/about', function () {
-  return view('about', ['nama' => 'Tyo']);
+  return view('about', ['header' => 'About Page']);
 });
 
 Route::get('/blog', function () {
-  return view('blog');
+  return view('blog', ['header' => 'Blog Page']);
 });
 
 Route::get('/contact', function () {
-  return view('contact');
+  return view('contact', ['header' => 'Contact Page']);
 });
