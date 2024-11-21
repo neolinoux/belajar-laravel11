c5304205fead40a5c2c8ebdb56af41937837a012
Rahmad Setiya Budi <222011623@stis.ac.id>
2024-11-22 05:03:51 +0800

    laravel 11 wpu part 13

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::create([
+            'name' => 'Laravel',
+            'slug' => 'laravel',
+        ]);
+        
+        Category::create([
+            'name' => 'React',
+            'slug' => 'react',
+        ]);
+
+        Category::create([
+            'name' => 'Viu',
+            'slug' => 'viu',
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +18,41 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // User::factory()->create([
+        //   'name' => fake()->name(),
+        //   'username' => fake()->userName(),
+        //   'email' => fake()->email(),
+        //   'email_verified_at' => now(),
+        //   'password' => Hash::make('password'),
+        //   'remember_token' => Str::random(10),
+        // ]);
+
+        // Category::factory()->create([
+        //   'name' => fake()->sentence(rand(1, 3), false),
+        //   'slug' => Str::slug(fake()->sentence(rand(1, 3), false)),
+        // ]);
+
+        // Post::factory()->create([
+        //   'title' => fake()->sentence(rand(1, 3), false),
+        //   'slug' => Str::slug(fake()->sentence(rand(1, 3), false)),
+        //   'author_id' => User::first()->id,
+        //   'category_id' => Category::first()->id,
+        //   'body' => fake()->paragraphs(rand(3, 7), true),
+        // ]);
+
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class
         ]);
+
+        Post::factory(100)->recycle(
+          Category::all(),
+          User::all()
+        )->create();
     }
 }
