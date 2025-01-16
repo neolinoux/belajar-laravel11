18dafcdc4e6b1d3bd9166a661ec56b25f21eb95b
Rahmad Setiya Budi <222011623@stis.ac.id>
2025-01-16 21:43:57 +0800

    wpu laravel 11 part 15

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,10 +3,10 @@
     <h1>Posts Page</h1>
     {{-- {{ dd($posts) }} --}}
     <section class="bg-white dark:bg-gray-900">
-        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-            <div class="grid gap-8 lg:grid-cols-2">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-4 lg:px-0">
+            <div class="grid gap-8 lg:grid-cols-3 md:grid-cols-2">
                 @foreach ($posts as $post)
-    {{-- 
+                    {{-- 
     <article class="py-8 max-w-screen-md border-b border-gray-300">
       <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
@@ -24,33 +24,41 @@
                     <article
                         class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                         <div class="flex justify-between items-center mb-5 text-gray-500">
-                            <span
-                                class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                                <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg">
+                            <a href="/categories/{{ $post->category->slug }}">
+                                <span
+                                    class="bg-{{ $post->category->color }}-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
                                     <path
                                         d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z">
                                     </path>
-                                </svg>
-                                Tutorial
+                                    </svg>
+                                    {{ $post->category->name }}
+                                </span>
+                            </a>
+                            <span class="text-sm">
+                                <?php
+                                // echo date_diff($post->created_at, now())->format('%d days ago');
+                                ?>
+                                {{ $post->created_at->diffForHumans() }}
                             </span>
-                            <span class="text-sm">14 days ago</span>
                         </div>
-                        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a
-                                href="#">How to quickly deploy a static website</a></h2>
-                        <p class="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to
-                            bootstrap lots of websites and are becoming the basis for a variety of tools that even
-                            influence both web designers and developers influence both web designers and developers.</p>
+                        <a href="/posts/{{ $post->slug }}" class="hover:underline">
+                            <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:underline">
+                                {{ $post->title }}
+                            </h2>
+                        </a>
+                        <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
+                            {{ Str::limit($post->body, 150) }}
+                        </p>
                         <div class="flex justify-between items-center">
                             <div class="flex items-center space-x-4">
                                 <img class="w-7 h-7 rounded-full"
                                     src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
                                     alt="Jese Leos avatar" />
-                                <span class="font-medium dark:text-white">
-                                    Jese Leos
+                                <span class="font-medium dark:text-white text-sm">
+                                    {{ $post->author->name }}
                                 </span>
                             </div>
-                            <a href="#"
+                            <a href="/posts/{{ $post->slug }}"
                                 class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
                                 Read more
                                 <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
